16100188b19795f6e7e8ac76dc4e52826c44a5a9
Ethan Edmond <eedmond@uccs.edu>
2021-07-21 13:02:48 -0600

    Finished test[15]

--- index.test.js
+++ index.test.js
@@ -109,7 +109,11 @@ describe('[Exercise 6] Car', () => {
   beforeEach(() => {
     focus = new utils.Car('focus', 20, 30); // each test must start with a fresh car
   });
-  test.todo('[15] driving the car returns the updated odometer');
+  test('[15] driving the car returns the updated odometer', () => {
+    const updatedOdometer = focus.drive(600);
+    expect(focus.odometer).toBe(600);
+    expect(updatedOdometer).toBe(focus.odometer);
+  });
   test.todo('[16] driving the car uses gas');
   test.todo('[17] refueling allows to keep driving');
   test.todo('[18] adding fuel to a full tank has no effect');
